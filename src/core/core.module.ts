7aea9e9a3fd457d6ec94e3ff7a31fb3ee0920b84
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-03 20:41:01 +0300

    fix:[Auth][Vadim]: fix core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './auth/auth.controller'
 import { AuthService } from './auth/auth.service'
+import { UserController } from './user/user.controller'
+import { UserService } from './user/user.service'
 
 @Module({
-	controllers: [AuthController],
-	providers: [AuthService],
-	exports: [AuthService]
+	controllers: [AuthController, UserController],
+	providers: [AuthService, UserService],
+	exports: []
 })
 export class CoreModule {}
