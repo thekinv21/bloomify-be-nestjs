8019b717456bbc57eb2fdc51b02c72863433017c
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-03 20:44:36 +0300

    feat:[Auth][Vadim]: create user controller

---
+++ src/core/user/user.controller.ts
@@ -0,0 +1,62 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { UUID } from 'crypto'
+import { AuthResponseDto } from '../auth/dto/auth.response'
+import { CreateUserDto, UpdateUserDto } from './dto/user.request'
+import { UserDto } from './dto/user.response'
+import { UserService } from './user.service'
+
+@ApiTags('User')
+@Controller('/user')
+@UsePipes(new ValidationPipe())
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Get()
+	async getAll(): Promise<UserDto[] | null> {
+		return this.userService.getAll()
+	}
+
+	@Get(':id')
+	async getById(@Param('id') id: UUID): Promise<UserDto | null> {
+		return this.userService.getById(id)
+	}
+
+	@Get(':username')
+	async getByUsername(
+		@Param('username') username: string
+	): Promise<UserDto | null> {
+		return this.userService.getByUsername(username)
+	}
+
+	@Post()
+	async create(@Body() dto: CreateUserDto): Promise<AuthResponseDto | null> {
+		return this.userService.create(dto)
+	}
+
+	@Put()
+	async update(@Body() dto: UpdateUserDto): Promise<UserDto | null> {
+		return this.userService.update(dto)
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: UUID): Promise<void> {
+		return this.userService.delete(id)
+	}
+
+	@Patch(':id')
+	async toggle(@Param('id') id: UUID): Promise<void> {
+		return this.userService.toggle(id)
+	}
+}
