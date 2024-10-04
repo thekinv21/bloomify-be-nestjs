6114e7b600bb54f47ae3276fa7c0511323068266
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-05 00:27:11 +0300

    feat:[Auth][Vadim]: custom api response eklendi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import type { NestExpressApplication } from '@nestjs/platform-express'
 import { SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { ApiResponseInterceptor } from './common/interceptors/response.interceptor'
 import { swagger } from './config/swagger.config'
 
 async function bootstrap() {
@@ -11,6 +12,8 @@ async function bootstrap() {
 
 	const document = SwaggerModule.createDocument(app, swagger)
 	SwaggerModule.setup('docs', app, document)
+
+	app.useGlobalInterceptors(new ApiResponseInterceptor())
 
 	await app.listen(process.env.PORT || 4200)
 }
