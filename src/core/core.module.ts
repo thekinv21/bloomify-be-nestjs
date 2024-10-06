fba81d7abbe5cc884a88f051293a16fa04951d2e
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-06 16:49:32 +0300

    fix:[jwt][Vadim]: jwt auth service core'a module dahil edildi

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
 import { PrismaService } from 'prisma/prisma.service'
 import { AuthController } from './auth/auth.controller'
 import { AuthService } from './auth/auth.service'
+import { JwtAuthService } from './auth/jwt/jwt.service'
 import { UserController } from './user/user.controller'
 import { UserService } from './user/user.service'
 
 @Module({
+	imports: [ConfigModule.forRoot()],
 	controllers: [AuthController, UserController],
-	providers: [AuthService, UserService, PrismaService],
+	providers: [
+		AuthService,
+		UserService,
+		PrismaService,
+		JwtService,
+		JwtAuthService
+	],
 	exports: []
 })
 export class CoreModule {}
