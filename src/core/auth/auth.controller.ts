7e14686ab85a9e90cf3f80acfe79dfe8877044d1
Vadim Kiniabaev <thekinv21@gmail.com>
2024-10-06 16:50:44 +0300

    feat:[jwt][Vadim]: auth controllerlar düzenlendi ve servislere gidecek şekilde ayarlandı

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -9,6 +9,7 @@ import {
 import { ApiTags } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
 import { LoginDto, RefreshTokenDto, RegisterDto } from './dto/auth.request'
+import { AuthResponseDto, TokenResponse } from './dto/auth.response'
 
 @ApiTags('Auth')
 @UsePipes(new ValidationPipe())
@@ -17,22 +18,22 @@ export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post('/register')
-	async register(@Body() dto: RegisterDto): Promise<string> {
+	async register(@Body() dto: RegisterDto): Promise<AuthResponseDto | null> {
 		return this.authService.register(dto)
 	}
 
 	@Post('/login')
-	async login(@Body() dto: LoginDto): Promise<string> {
+	async login(@Body() dto: LoginDto): Promise<AuthResponseDto | null> {
 		return this.authService.login(dto)
 	}
 
 	@Post('/refresh-token')
-	async refreshToken(@Body() dto: RefreshTokenDto): Promise<string> {
+	async refresh(@Body() dto: RefreshTokenDto): Promise<TokenResponse | null> {
 		return this.authService.refreshToken(dto)
 	}
 
-	@Get('/validate')
-	async validateUser(): Promise<string> {
-		return this.authService.validateUser()
+	@Get('/logout')
+	async logout(): Promise<void> {
+		return this.authService.logout()
 	}
 }
